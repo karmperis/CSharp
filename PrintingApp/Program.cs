bb470938d956fd749269459d605d8942859be4bf
Nikolaos Karmperis <karmperis@gmail.com>
2026-03-09 19:36:30 +0200

    chore: add basic output logic

---
+++ PrintingApp/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace PrintingApp
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int num = 0;
+			Console.WriteLine("Hello, World!");
+			Console.WriteLine("Num = {0}", num); // placeholder
+			Console.WriteLine($"Num = {num}"); // interpolation
+		}
+	}
+}
