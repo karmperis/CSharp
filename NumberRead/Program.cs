5d515be09fbab7fc8783e2c63c391f8bbd39a560
Nikolaos Karmperis <karmperis@gmail.com>
2026-03-09 19:57:57 +0200

    chore: add read logic with tryparse

---
+++ NumberRead/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace NumberRead
+{
+	/// <summary>
+	/// Safe reading of float and double numbers from the console using TryParse method to handle invalid input gracefully.
+	/// </summary>
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			float floatNum = 0F;
+			double doubleNum = 0D;
+
+			Console.WriteLine("Enter a float number:");
+
+			if(!float.TryParse(Console.ReadLine(), out floatNum))
+			{
+				Console.WriteLine("Invalid input for float number.");
+				return;
+			}
+
+			if(!double.TryParse(Console.ReadLine(), out doubleNum))
+			{
+				Console.WriteLine("Invalid input for double number.");
+				return;
+			}
+
+			Console.WriteLine($"You entered float: {floatNum,6:N2}");
+			Console.WriteLine($"You entered double: {doubleNum,6:N2}");
+
+		}
+	}
+}
