da07d00ec6f02e3714ef79664d352b07fea99a5c
Nikolaos Karmperis <karmperis@gmail.com>
2026-03-11 17:09:03 +0200

    chore: add do while logic

---
+++ DoWhileApp/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace DoWhileApp
+{
+	/// <summary>
+	/// Counts the number of digits of an int.
+	/// </summary>
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int num = 0;
+			int numberOfDigits = 0;
+			int tmp = 0;
+
+			Console.WriteLine("Please insert an integer");
+			if (!int.TryParse(Console.ReadLine(), out num))
+			{
+				Console.WriteLine("Invalid insert!!! Please insert an integer");
+				return;
+			}
+
+			tmp = num;
+			do
+			{
+				tmp /= 10;
+				numberOfDigits++;
+			} while (tmp != 0);
+
+			Console.WriteLine($"Digits count: {numberOfDigits}");
+		}
+	}
+}
